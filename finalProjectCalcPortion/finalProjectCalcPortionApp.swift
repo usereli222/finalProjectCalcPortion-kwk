66ac58613643a2db596f8456326c806f0b16720d
emily <eli.mhs24@gmail.com>
2023-07-18 13:42:08 -0500

    Initial Commit

---
+++ finalProjectCalcPortion/finalProjectCalcPortionApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectCalcPortionApp.swift
+//  finalProjectCalcPortion
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectCalcPortionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
